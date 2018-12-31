b94923219734a35632056755f85417f42cd7d368
ray <ray@rayanticlimatic@gmail.com>
2018-12-31 21:00:53 +0900

    dict_maker update

--- dataset_maker.py
+++ dataset_maker.py
@@ -7,13 +7,15 @@ out_ = [] #listener's response
 utters = {} #dictionary of all the dialogues with dialogue number as keys
 keys = [] #stores key pair of speaker and listener's dialogue
 
-def line_to_dic(): #
+def line_to_dic(): 
     with codecs.open("cornell movie-dialogs corpus/movie_lines.txt","r", "Shift-JIS", "ignore") as f: #ignores multibyted characters
         lines = [s.strip() for s in f.readlines()] #splits txt data by lines
         for line in lines:
             tag = line.split(" +++$+++ ")[0] #the first section is the key for the dialogue
             utter = line.split(" +++$+++ ")[-1] #the last section is the dialogue
-            utters[tag] = utter #connects the key and the dialogue
+            utter_set = set(utter) #multibyte letters and special characters are not wanted
+            if utter_set <= set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','.',',','\"','\'','!','?',"-",";"," "]): 
+                utters[tag] = utter #connects the key and the dialogue
 
 def conversation_key_pair_maker():
     with codecs.open("cornell movie-dialogs corpus/movie_conversations.txt","r", "Shift-JIS", "ignore") as f: #ignores multibyted characters
@@ -28,9 +30,13 @@ def conversation_key_pair_maker():
 
 def conversation_pair_maker(): #uses the key pair to make a list of the speaker's and the listener's dialogue
     for key_pair in keys:
-        in_key, out_key = key_pair
-        in_.append(utters[in_key])
-        out_.append(utters[out_key])
+        try:
+            in_key, out_key = key_pair
+            in_value, out_value = utters[in_key], utters[out_key]
+            in_.append(in_value)
+            out_.append(out_value)
+        except KeyError:
+            continue
 
 #executes the preceeding three functions
 line_to_dic()
--- dict_maker.py
+++ dict_maker.py
@@ -8,23 +8,27 @@ with open("in_.pickle", mode='rb') as f:
 with open("out_.pickle", mode='rb') as f:
     out_ = pickle.load(f)
 
-all = in_ + [out_[-1]]
+all_ = in_ + [out_[-1]] #all_ contains all the dialogue
 
-vocab = set([])
-for i in all:
-    c = set(i)
-    vocab = vocab | c
-vocab.remove(" ")
+vocab = set([]) #vocab contains all the vocabulary
+for dialogue in all_:
+    c_list = dialogue.split(" ")
+    c = set(c_list)
+    vocab = vocab | c #not to make a duplication in the vocab list
 
-vocab = list(vocab)
+vocab = list(vocab) #list is more convenient; it is ordered
+vocab.sort() #sorts the vocab
+vocab_length = len(vocab) #number of words in the vocabulary
+print(vocab_length,vocab[100])
 
-vocab_dict = dict(zip(vocab, list(range(1, 2362+1))))
-vocab_dict["§"] = 0
-vocab_dict[" "] = 1
-r_vocab_dict = dict(zip(list(range(1, 2362+1)), vocab))
-r_vocab_dict[0] = "§"
-r_vocab_dict[1] = " "
+vocab_dict = dict(zip(vocab, list(range(2, vocab_length+1)))) #key:word element:integer
+vocab_dict["§"] = 0 #repersents "EOS"(End of Statement)
+vocab_dict[" "] = 1 #represents "space"
+r_vocab_dict = dict(zip(list(range(2, vocab_length+1)), vocab)) #key:integer element:word
+r_vocab_dict[0] = "§" #repersents "EOS"(End of Statement)
+r_vocab_dict[1] = " " #represents "space"
 
+# stores the dictionaries in pickle format
 with open("vocab_dict.pickle", mode='wb') as f:
     pickle.dump(vocab_dict, f)
 with open("r_vocab_dict.pickle", mode='wb') as f:
